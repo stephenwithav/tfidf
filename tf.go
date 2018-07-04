dcff3fef3c5db4c97ebf0a96a32c73d0df5ae8d2
Steven Edwards <steven@stephenwithav.com>
2018-07-04 10:37:15 -0400

    Add termCount func, along with helpers scanAlphaNum, isValid, and tests
    termCount is a private function for now, as are scanAlphaNum an
    isValid.
    
    It likely become public to demonstrate how the final interface is
    implemented.
    
    isValid will definitely be renamed isAlphaNum.

---
+++ tf.go
@@ -0,0 +1,72 @@
+package tfidf
+
+import (
+	"bufio"
+	"io"
+	"strings"
+	"unicode"
+	"unicode/utf8"
+)
+
+// scanAlphaNum is a split function for a Scanner that returns each
+// alphanumeric token of text, with surrounding spaces deleted. It will
+// never return an empty string.
+func scanAlphaNum(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	// Skip invalid leading characters.
+	start := 0
+	for width := 0; start < len(data); start += width {
+		var r rune
+		r, width = utf8.DecodeRune(data[start:])
+		if isValid(r) {
+			break
+		}
+
+	}
+
+	// Scan until invalid character.
+	for width, i := 0, start; i < len(data); i += width {
+		var r rune
+		r, width = utf8.DecodeRune(data[i:])
+		if !isValid(r) {
+			return i + width, data[start:i], nil
+		}
+
+	}
+	// If we're at EOF, we have a final, non-empty, non-terminated word. Return it.
+	if atEOF && len(data) > start {
+		return len(data), data[start:], nil
+	}
+
+	// Request more data.
+	return start, nil, nil
+}
+
+// isValid is a function that tells whether a rune is alphanumeric.
+func isValid(r rune) bool {
+	return unicode.IsLetter(r) || unicode.IsNumber(r)
+}
+
+// termCount is a function that returns the term-frequency of the given io.Reader.
+// No stop-words are eliminated yet.
+func termCount(doc io.Reader) map[string]float64 {
+	scanner := bufio.NewScanner(doc)
+	scanner.Split(scanAlphaNum)
+
+	// We start with no words.
+	termCount := 0
+	terms := make(map[string]int)
+
+	// Count the term totals.  TODO: Add an if-block.  if tok, invalid := invalidTokens[lowerCasedText]; !invalid { code here }
+	for scanner.Scan() {
+		terms[strings.ToLower(scanner.Text())]++
+		termCount++
+	}
+
+	// Now calculate their frequency as a percentage.
+	tFrequency := make(map[string]float64, len(terms))
+	for term, count := range terms {
+		tFrequency[term] = float64(count) / float64(termCount)
+	}
+
+	return tFrequency
+}
---
+++ tf_test.go
@@ -0,0 +1,28 @@
+package tfidf
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestTermCount(t *testing.T) {
+	given := []string{
+		"Happy easter, Easter Bunny",
+		"President George Washington was the first American President",
+	}
+
+	expected := []map[string]float64{
+		map[string]float64{"happy": 1 / 4.0, "easter": 2 / 4.0, "bunny": 1 / 4.0},
+		map[string]float64{"president": 2 / 8.0, "american": 1 / 8.0, "george": 1 / 8.0, "washington": 1 / 8.0, "was": 1 / 8.0, "the": 1 / 8.0, "first": 1 / 8.0},
+	}
+
+	for i, s := range given {
+		found := TermCount(strings.NewReader(s))
+
+		for term, freq := range found {
+			if freq != expected[i][term] {
+				t.Errorf("got %f, expected %f\t[%s]", freq, expected[i][term], term)
+			}
+		}
+	}
+}
